8a4fb96e7556b2dfc1fb86aa2c304a3f7e59dc57
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-14 05:38:16 -0500

    obtener datos usuario

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import FundSubscription from './components/FundSubscription';
@@ -7,6 +7,33 @@ import TransactionHistory from './components/TransactionHistory';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/users/1'); // Ajustar URL según tu configuración de API
+        if (!response.ok) {
+          throw new Error('Error al cargar los datos del usuario');
+        }
+        const userData = await response.json();
+        setUserData(userData);
+      } catch (error) {
+        console.error('Error al obtener datos del usuario:', error.message);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP'
+    }).format(amount);
+  };
+
   return (
     <Router>
       <AppBar position="static">
@@ -14,6 +41,11 @@ function App() {
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             Gestor de fondos
           </Typography>
+
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+
           <Button color="inherit" component={Link} to="/subscribe">
             Suscribirse
           </Button>
@@ -32,7 +64,8 @@ function App() {
           <Route path="/history" element={<TransactionHistory />} />
           <Route path="/" element={
             <Typography variant="h4" align="center" gutterBottom>
-              Bienvenido al Fondo Voluntario de Pensión
+              Hola
+              {/* Hola {userData.name}, disponible: {formatCurrency(userData.amount)} { } */}
             </Typography>
           } />
         </Routes>
