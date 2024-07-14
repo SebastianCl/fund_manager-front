38b4517567cf536a3a7f40da1fd8a4616394ff94
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-14 06:05:53 -0500

    pop up cancelacion de fondo

--- src/components/FundCancellation.js
+++ src/components/FundCancellation.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from '@mui/material';
 
 function FundCancellation() {
     const [transactionId, setTransactionId] = useState('');
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogContent, setDialogContent] = useState({});
 
     const handleCancellation = () => {
         const cancellationData = {
@@ -13,10 +15,32 @@ function FundCancellation() {
         axios.post('http://localhost:8000/cancel_a_found', cancellationData)
             .then(response => {
                 console.log('Cancelled successfully:', response.data);
+                setDialogContent({
+                    title: 'Cancelación Exitosa',
+                    message: '¡Se ha cancelado la suscripción al fondo correctamente!'
+                });
+                setDialogOpen(true);
             })
             .catch(error => {
                 console.error('Error cancelling subscription:', error);
+                if (error.response && error.response.data && error.response.data.detail) {
+                    setDialogContent({
+                        title: 'Error de Cancelación',
+                        message: `Error: ${error.response.data.detail}`
+                    });
+                } else {
+                    setDialogContent({
+                        title: 'Error de Cancelación',
+                        message: 'Hubo un error al intentar cancelar la suscripción.'
+                    });
+                }
+                setDialogOpen(true);
             });
+    };
+
+    const handleCloseDialog = () => {
+        setDialogOpen(false);
+        setDialogContent({});
     };
 
     return (
@@ -33,6 +57,18 @@ function FundCancellation() {
             <Button variant="contained" color="primary" onClick={handleCancellation} fullWidth>
                 Cancelar Suscripción
             </Button>
+
+            <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+                <DialogTitle>{dialogContent.title}</DialogTitle>
+                <DialogContent>
+                    <Typography>{dialogContent.message}</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 }
