aefdf159500558696c24103130b9f8a1b4b14f65
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-22 02:46:08 -0500

    componente home

---
+++ src/adapters/ui/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+
+const Home = () => {
+    return (
+        <Typography variant="h4" align="center" gutterBottom>
+            Bienvenido al gestor de fondos
+        </Typography>
+    );
+};
+
+export default Home;
--- src/adapters/ui/containers/App.js
+++ src/adapters/ui/containers/App.js
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import FundSubscription from '../components/FundSubscription';
 import FundCancellation from '../components/FundCancellation';
 import UserHeader from '../components/UserHeader';
+import Home from '../components/Home';
 import TransactionHistory from '../components/TransactionHistory';
-import userRepository from '../../../ports/userRepository';
 import '../../../App.css';
 
 function App() {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const fetchUser = async () => {
-            const userData = await userRepository.getUserById(1);
-            setUser(userData);
-        };
-
-        fetchUser();
-    }, []);
 
     return (
         <Router>
@@ -27,6 +17,9 @@ function App() {
                     <Typography variant="h6" style={{ flexGrow: 1 }}>
                         <UserHeader />
                     </Typography>
+                    <Button color="inherit" component={Link} to="/">
+                        Inicio
+                    </Button>
                     <Button color="inherit" component={Link} to="/subscribe">
                         Suscribirse
                     </Button>
@@ -43,11 +36,7 @@ function App() {
                     <Route path="/subscribe" element={<FundSubscription />} />
                     <Route path="/cancel" element={<FundCancellation />} />
                     <Route path="/history" element={<TransactionHistory />} />
-                    <Route path="/" element={
-                        <Typography variant="h4" align="center" gutterBottom>
-                            Bienvenido al Fondo Voluntario de Pensión
-                        </Typography>
-                    } />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </Container>
         </Router>
