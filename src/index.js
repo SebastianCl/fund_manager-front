7996af1166d0e56cf4caf643f03fcffb91bc089f
Sebastian Cardona <cardonaloaizasebastian112@gmail.com>
2024-07-22 01:09:56 -0500

    actualizacion ReactDOM.createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './adapters/ui/containers/App';
 import userRepository from './ports/userRepository';
 import userRepositoryAPI from './adapters/api/userRepositoryAPI';
 import transactionRepository from './ports/transactionRepository';
 import transactionRepositoryAPI from './adapters/api/transactionRepositoryAPI';
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 // Inyectar la implementación del repositorio
 userRepository.getUserById = userRepositoryAPI.getUserById;
 transactionRepository.getTransactions = transactionRepositoryAPI.getTransactions;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+reportWebVitals();
